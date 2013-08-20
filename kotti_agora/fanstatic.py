d3d5e330a032d267513d1c0c4b5ed4bb1a617e07
Mike Cullerton <webstuff@bakednotfried.com>
2013-08-19 21:50:35 -0600

    more stuff for batching

--- kotti_agora/fanstatic.py
+++ kotti_agora/fanstatic.py
@@ -4,6 +4,20 @@ from fanstatic import Group
 from fanstatic import Library
 from fanstatic import Resource
 
+from js.jquery_infinite_ajax_scroll import jquery_infinite_ajax_scroll
+from js.jquery_infinite_ajax_scroll import jquery_infinite_ajax_scroll_css
+
+from kotti.fanstatic import view_needed
+
 library = Library("kotti_agora", "static")
-kotti_agora_css = Resource(library, "style.css")
-kotti_agora_group = Group([kotti_agora_css])
+kotti_agora_css = Resource(library, 
+    "style.css",
+    depends=[jquery_infinite_ajax_scroll_css, ],
+    minified='style.min.css',
+    bottom=True)
+kotti_agora_js = Resource(library, 
+    "kotti_agora.js",
+    depends=[jquery_infinite_ajax_scroll, ],
+    minified='kotti_agora.min.js',
+    bottom=True)
+kotti_agora_group = Group([kotti_agora_css,kotti_agora_js])
